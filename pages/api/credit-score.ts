684b0359cea6c1759cb503a4f4c3c2e15475efe4
Jack Servia <jackryanservia@gmail.com>
2022-09-15 15:53:55 -0600

    Added credit-score API

---
+++ pages/api/credit-score.ts
@@ -0,0 +1,25 @@
+import { isReady, PublicKey, PrivateKey, Field, Signature } from "snarkyjs";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  publicKey: PublicKey;
+  creditScore: Field;
+  signature: Signature;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await isReady;
+
+  const privateKey = PrivateKey.fromBase58(process.env.PRIVATE_KEY);
+  const publicKey = privateKey.toPublicKey();
+
+  const randomCreditScore = () => Math.floor(Math.random() * 551 + 300);
+  const creditScore = Field(randomCreditScore());
+
+  const signature = Signature.create(privateKey, [creditScore]);
+
+  res.status(200).json({ publicKey, creditScore, signature });
+}
