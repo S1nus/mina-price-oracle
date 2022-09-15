49cf9f1e4a0ea1c0d36145c475ca59a992bfc821
Jack Servia <jackryanservia@gmail.com>
2022-09-15 15:59:01 -0600

    Fixed private key environment variable

--- pages/api/credit-score.ts
+++ pages/api/credit-score.ts
@@ -13,7 +13,10 @@ export default async function handler(
 ) {
   await isReady;
 
-  const privateKey = PrivateKey.fromBase58(process.env.PRIVATE_KEY);
+  const privateKey = PrivateKey.fromBase58(
+    process.env.PRIVATE_KEY ??
+      "EKF65JKw9Q1XWLDZyZNGysBbYG21QbJf3a4xnEoZPZ28LKYGMw53"
+  );
   const publicKey = privateKey.toPublicKey();
 
   const randomCreditScore = () => Math.floor(Math.random() * 551 + 300);
